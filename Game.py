d288c24e1b40141a4dad5ae6ab392d8a8ab6dd7b
ashie-21 <95080529+ashie-21@users.noreply.github.com>
2021-12-03 22:00:08 +0530

    Update Game.py

--- Game.py
+++ Game.py
@@ -3,14 +3,19 @@ number = random.randint(1, 9)
 print ("Number Guessing Game")
 guess = input("Guess a no. (between 1 and 9):")
 # Compare the user entered number with the number to be guessed
-if guess == number:
-    # if number entered by user is same as the generated
-    # number by randint function then break from loop using loop
-    # control statement "break"
-    print ("Congratulation YOU WON!!!")
-    break
-# Check whether the user guessed the correct number
-if not chances < 5:
-    print("YOU LOSE!!! The number is", number)
+
+if guess<number:
+    print("Oh! The no. is too small make another guess")
+if guess>number:
+    print("oh! the no. is too big")
+
 # While loop to count the number of chances
 while chances < 5:
+    if guess == number:
+        # if number entered by user is same as the generated
+        # number by randint function then break from loop using loop
+        # control statement "break"
+        print ("Congratulation YOU WON!!!")
+        # Check whether the user guessed the correct number
+    if not chances < 5:
+        print("YOU LOSE!!! The number is", number)
